0bd03727e4d87ad83a805917ef105c7e22310018
smad2005 <smad2005@gmail.com>
2014-04-15 19:26:59 +0300

    Update ConvertorHex.cs

--- HexToString/ConvertorHex.cs
+++ HexToString/ConvertorHex.cs
@@ -5,24 +5,34 @@ using System.Text;
 
 namespace HexToString
 {
-    public class ConvertorHex
+public static class ConvertorHex
     {
-        public string StringToHex(string text, Encoding encoding)
+        public static string BytesToHex(Byte [] bytes)
         {
-            return String.Join(String.Empty, encoding.GetBytes(text).Select(y => y.ToString("x")));
+            return string.Concat(bytes.Select(y => y.ToString("x2")).ToArray());
         }
 
-        public string StringToHex(string text)
+        public static string StringToHex(string text, Encoding encoding)
+        {
+            return BytesToHex(encoding.GetBytes(text));
+        }
+
+        public static string StringToHex(string text)
         {
             return StringToHex(text, Encoding.Default);
         }
 
-        public string HexToString(string hexTex, Encoding encoding)
+        public static byte[] HexToBytes(string hexTex)
         {
-            return String.IsNullOrEmpty(hexTex)? String.Empty: encoding.GetString(Enumerable.Range(0, hexTex.Length - 1).Where(y => (y & 1) == 0).Select(y => Byte.Parse(hexTex[y] + hexTex[y + 1].ToString(), NumberStyles.HexNumber)).ToArray());
+            return String.IsNullOrEmpty(hexTex) ?null :Enumerable.Range(0, hexTex.Length - 1).Where(y => (y & 1) == 0).Select(y => Byte.Parse(hexTex[y] + hexTex[y + 1].ToString(), NumberStyles.HexNumber)).ToArray();
         }
 
-        public string HexToString(string hexTex)
+        public static string HexToString(string hexTex, Encoding encoding)
+        {
+            return String.IsNullOrEmpty(hexTex) ? String.Empty : encoding.GetString(HexToBytes(hexTex));
+        }
+
+        public static string HexToString(string hexTex)
         {
             return HexToString(hexTex, Encoding.Default);
         }
